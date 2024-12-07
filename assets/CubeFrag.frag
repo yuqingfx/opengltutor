bf562257d7c0674a9237a2676a577be195f4d969
yuqingfx@gmail.com <Chen8365707~>
2024-12-07 12:15:44 +0800

    done emission map

--- assets/CubeFrag.frag
+++ assets/CubeFrag.frag
@@ -29,6 +29,7 @@ in vec2 TexCoord;
 uniform vec3 lightColor;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
+uniform float time;
 
 void main()
 {
@@ -48,10 +49,10 @@ void main()
 	vec3 specular = light.specular * spec * vec3(texture(mat.specular, TexCoord));
 
 	// emission
-	vec3 emission = texture(mat.emission, TexCoord).rgb;
-
+	
+	float ds = 0.125;
+	vec3 emission = texture(mat.emission, TexCoord * (1.0 + ds * 2) - ds).rgb;
+	emission = emission * (sin(time) * 0.5 + 0.5) * 2.0;
 	vec3 result = ambient + diffuse + specular + emission;
-
 	FragColor = vec4(result, 1.0);
-
 } 
--- src/main.cpp
+++ src/main.cpp
@@ -290,6 +290,8 @@ int main() {
         CubeShaderProgram.setVec3("viewPos", camera.Position);
 
         // ps stage
+        CubeShaderProgram.setFloat("time", glfwGetTime());
+
         CubeShaderProgram.setVec3("light.ambient", LightIntensity);
         CubeShaderProgram.setVec3("light.diffuse", LightColor);
         CubeShaderProgram.setVec3("light.specular", LightSpecIntensity);
@@ -441,8 +443,8 @@ unsigned int loadTexture(std::string InAssetPath)
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, TexData);
         glGenerateMipmap(GL_TEXTURE_2D);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
