a894210861ae2685b135c464ec08c98de91533fd
yuqing <yuqingfx@gmail.com>
2024-12-19 23:07:05 +0800

    WIP mesh

--- src/DataStructures.h
+++ src/DataStructures.h
@@ -81,6 +81,20 @@ private:
 
 	void setupMesh()
 	{
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+	
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+
+
 
 	};
 
